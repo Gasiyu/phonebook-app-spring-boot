aefa8ea5cfd689b802c51fa3c68ecc0e5b86cbf6
Gasiyu <gasiyu@proton.me>
2025-07-07 16:25:53 +0700

    feat(phonebook): add User entity
    - create User entity with fields: id, username, password, role, and isActive

---
+++ src/main/kotlin/org/hyperskill/phonebook/model/User.kt
@@ -0,0 +1,25 @@
+package org.hyperskill.phonebook.model
+
+import jakarta.persistence.*
+import java.util.*
+
+@Entity
+@Table(name = "user")
+data class User(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: UUID? = null,
+    
+    @Column(nullable = false, unique = true)
+    var username: String,
+    
+    @Column(nullable = false)
+    var password: String,
+    
+    @Column(nullable = false)
+    var role: String,
+    
+    @Column(name = "is_active", nullable = false)
+    var isActive: Boolean = true
+)
