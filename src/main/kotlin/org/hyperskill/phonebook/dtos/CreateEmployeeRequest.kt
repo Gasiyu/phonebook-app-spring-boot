91f893c0dd781f2e56d15512f1c5d21b9996206b
Gasiyu <gasiyu@proton.me>
2025-07-11 18:14:57 +0700

    feat(controller): add employee creation endpoint
    - implement POST endpoint to create a new employee

--- src/main/kotlin/org/hyperskill/phonebook/dtos/CreateEmployeeRequest.kt
+++ src/main/kotlin/org/hyperskill/phonebook/dtos/CreateEmployeeRequest.kt
@@ -1,11 +1,22 @@
 package org.hyperskill.phonebook.dtos
 
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
 import java.util.*
 
 data class CreateEmployeeRequest(
+    @field:NotBlank
     val name: String,
+
+    @field:NotBlank
     val position: String,
+
+    @field:NotBlank
     val phone: String,
+
+    @field:Email
     val email: String?,
+
+    @field:org.hibernate.validator.constraints.UUID
     val departmentId: UUID?
 )
