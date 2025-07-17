561d672716c2090232a14ca957294702b3326ea2
camilo960321 <camilo123@outlook.it>
2025-07-17 19:45:52 +0200

    Cache implementation

---
+++ src/main/kotlin/org/hyperskill/phonebook/service/CacheImplementation.kt
@@ -0,0 +1,20 @@
+package org.hyperskill.phonebook.service
+
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.stereotype.Service
+
+@Service
+class UserphonebookService {
+
+    @Cacheable("cachePhoneBook") // ⬅️ Match alias in ehcache.xml
+    fun getUser(userId: Long): String {
+        println("Fetching from DB...")
+        return "User data for $userId"
+    }
+
+    @CacheEvict("cachePhoneBook", key = "#userId")
+    fun deleteUser(userId: Long) {
+        println("Evicting cache...")
+    }
+}
