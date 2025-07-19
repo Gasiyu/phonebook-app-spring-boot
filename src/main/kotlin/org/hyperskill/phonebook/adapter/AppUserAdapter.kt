24fba5cd7b8af6bfc8aff3cb458aae300b6129f8
Gasiyu <gasiyu@proton.me>
2025-07-20 06:39:11 +0700

    refactor(adapter): enhance logging and initialize logger in AppUserAdapter
    - replace println with LOGGER.debug for roles logging
    - add a companion object to initialize the Logger instance

--- src/main/kotlin/org/hyperskill/phonebook/adapter/AppUserAdapter.kt
+++ src/main/kotlin/org/hyperskill/phonebook/adapter/AppUserAdapter.kt
@@ -1,14 +1,17 @@
 package org.hyperskill.phonebook.adapter
 
 import org.hyperskill.phonebook.model.User
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 
 class AppUserAdapter(private val user: User) : UserDetails {
     override fun getAuthorities(): Collection<GrantedAuthority> {
-        println("User roles: ${user.roles}")
-        return user.roles.map { SimpleGrantedAuthority("ROLE_${it.name.uppercase()}") }
+        val userRoles = user.roles.map { SimpleGrantedAuthority("ROLE_${it.name.uppercase()}") }
+        LOGGER.debug("User roles: {}", userRoles)
+        return userRoles
     }
 
     override fun getPassword(): String = requireNotNull(user.password)
@@ -22,4 +25,8 @@ class AppUserAdapter(private val user: User) : UserDetails {
     override fun isCredentialsNonExpired(): Boolean = true
 
     override fun isEnabled(): Boolean = true
+
+    companion object {
+        private val LOGGER: Logger = LoggerFactory.getLogger(AppUserAdapter::class.java)
+    }
 }
