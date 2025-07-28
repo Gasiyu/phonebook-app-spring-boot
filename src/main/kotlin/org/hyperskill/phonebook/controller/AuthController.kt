e63ea8e27e52d4ef359d107adaf9624c12e6e559
Gasiyu <gasiyu@proton.me>
2025-07-28 08:27:21 +0700

    refactor(controller): remove unused imports and dependency from AuthController
    - eliminate unnecessary imports to clean up the code
    - remove userPhonebookService dependency for improved class focus

--- src/main/kotlin/org/hyperskill/phonebook/controller/AuthController.kt
+++ src/main/kotlin/org/hyperskill/phonebook/controller/AuthController.kt
@@ -1,22 +1,19 @@
 package org.hyperskill.phonebook.controller
 
+import org.hyperskill.phonebook.dtos.Request
+import org.hyperskill.phonebook.dtos.Response
 import org.hyperskill.phonebook.service.AuthService
 import org.hyperskill.phonebook.service.JwtService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.*
-import org.hyperskill.phonebook.service.UserPhonebookService
-import org.hyperskill.phonebook.dtos.Response
-import org.hyperskill.phonebook.dtos.Request
 
 @RestController
 @RequestMapping("api")
 class AuthController(
     private val authService: AuthService,
-    private val jwtService: JwtService,
-    private val userPhonebookService: UserPhonebookService
+    private val jwtService: JwtService
 ) {
     @PostMapping("login")
     fun login(@RequestBody body: Request): Response {
@@ -27,4 +24,3 @@ class AuthController(
         )
     }
 }
-
